9a9e0a504f8933feae02d576ea0b3660d52ba1fa
niko_laus <nik.tarnavsky123@gmail.com>
2025-09-17 19:14:08 +0300

    Added homework5

---
+++ home-work5/home-work5.3.js
@@ -0,0 +1,17 @@
+const num = 7;
+
+for(let i = 1; i <= 10; i++){
+    console.log(`${num} x ${i} = ${num * i}`)
+}
+
+
+console.log("---------------");
+
+
+let i = 1;
+const secondNum = 6;
+
+while (i <= 10){
+    console.log(`${secondNum} x ${i} = ${secondNum * i}`)
+    i++;
+}
